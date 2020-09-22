b783252f0780b32d5f2c2faf09818a88924bc63a
mactto <shc9928@gmail.com>
2020-09-23 02:05:57 +0900

    updates

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import "./App.css";
 import Row from "./Row";
 import requests from "./Requests";
 import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div className="App">
-      {/* Nav */}
+      <Nav />
       <Banner />
       <Row
         title="NETFLIX ORIGINALS"
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <img
+        className="nav_logo"
+        img="https://e7.pngegg.com/pngimages/119/342/png-clipart-logo-netflix-nasdaq-nflx-brand-television-copywriter-floor-television-text-thumbnail.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
